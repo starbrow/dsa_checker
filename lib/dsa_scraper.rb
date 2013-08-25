5ad3bfccd0feb4f38109f514ad3cfce51c86bf08
Elizabeth Reiff <e@starbrow.org>
2013-08-25 18:16:02 +0100

    refactoring code to separate out login from selecting date from choosing best date

--- lib/dsa_scraper.rb
+++ lib/dsa_scraper.rb
@@ -20,19 +20,30 @@ class DSAScraper
   def scraper_date
 
     agent = Mechanize.new
-    best_date = agent.get('https://www.gov.uk/change-date-practical-driving-test').link_with(:text => /Start now/).click.
-      form_with(:action => '/login') do |form|
-        form.username = ENV['DSADRIVERNUMBER']
-        form.password = ENV['DSAAPPOINTMENTNUMBER']
-      end.click_button.
-      link_with(:id => "date-time-change").click.
-      forms.first.submit.
-      at('.button-board a').text
 
-      if Date.parse(best_date) < @existing_date
-        best_date
-      else
-        false
-      end
+    manage_page = scraper_login(agent)
+    select_page = manage_page.link_with(:id => "date-time-change").click.
+      forms.first.submit
+
+    best_date = get_date(select_page)
+
+    if Date.parse(best_date) < @existing_date
+      best_date
+    else
+      false
     end
+  end
+
+  def scraper_login(agent)
+    agent.get('https://www.gov.uk/change-date-practical-driving-test').
+      link_with(:text => /Start now/).click.
+      form_with(:action => '/login') do |form|
+      form.username = ENV['DSADRIVERNUMBER']
+      form.password = ENV['DSAAPPOINTMENTNUMBER']
+      end.click_button
+  end
+
+  def get_date(page)
+    page.at('.button-board a').text
+  end
 end
